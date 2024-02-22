9a89e5bc053bfd26ba2e32db1fa9b20f7000ff56
Pedro Michel <pedromichel23@gmail.com>
2024-02-22 14:41:19 -0700

    create encrypt logic and web functionality

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,39 @@
+const textAreaInput = document.querySelector(".text-input")
+const btnEncrypt = document.querySelector(".btn-encrypt")
+const btnDecrypt = document.querySelector(".btn-decrypt")
+const btnCopyToClipboard = document.querySelector(".btn-text-copy")
+const txtResult = document.querySelector(".text-result")
+const txtInstructions = document.querySelector(".text-instructions")
 
-function encryptWord(word) {
-    const result = encrypt(word)
-    return result
+btnEncrypt.addEventListener('click', () => {
+    const input = textAreaInput.value.trim()
+    const result = encryptText(input)
+    txtResult.textContent = result
+    txtResult.style.fontWeight = 'normal'
+    txtResult.style.textAlign = 'left'
+    txtInstructions.style.display = 'none'
+    btnCopyToClipboard.classList.remove('hide')
+})
+
+btnCopyToClipboard.addEventListener('click',  () => {
+    const text = txtResult.textContent
+    navigator.clipboard.writeText(text)
+    .then( () => {
+        navigator.clipboard.readText()
+        .then((clipBoard) => {
+            console.log('clipboard:', clipBoard)
+            alert("Texto copiado: " + clipBoard)
+        })
+        // alert("Texto copiado")
+    })
+    // alert('Texto copiado al portapapeles.')
+    // console.log('Clipboard: ', navigator.clipboard.readText())
+})
+
+function encryptText(text) {
+    const words = text.split(' ')
+    const result = words.map(word => encrypt(word))
+    return result.join(' ')
 }
 
 function hasVowels(word) {
@@ -44,7 +76,7 @@ const word1 = "gato"
 const word2 = "manzana"
 const word3 = "mnzn"
 const word4 = "gátó"
-console.log(encryptWord(word1))
-console.log(encryptWord(word2))
-console.log(encryptWord(word3))
-console.log(encryptWord(word4))
+// console.log(encrypt(word1))
+// console.log(encrypt(word2))
+// console.log(encrypt(word3))
+// console.log(encrypt(word4))
